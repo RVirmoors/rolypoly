c822ad6a143b69047604402a5688951068b00f10
RVirmoors <gburloiu@gmail.com>
2020-06-11 00:14:14 +0300

    parse file, send osc

---
+++ py/constants.py
@@ -0,0 +1,22 @@
+# copied from https://github.com/tensorflow/magenta/blob/master/magenta/models/music_vae/data.py
+
+ROLAND_DRUM_PITCH_CLASSES = [
+    # kick drum
+    [36],
+    # snare drum
+    [38, 37, 40],
+    # closed hi-hat
+    [42, 22, 44],
+    # open hi-hat
+    [46, 26],
+    # low tom
+    [43, 58],
+    # mid tom
+    [47, 45],
+    # high tom
+    [50, 48],
+    # crash cymbal
+    [49, 52, 55, 57],
+    # ride cymbal
+    [51, 53, 59]
+]
--- py/roly.py
+++ py/roly.py
@@ -1,15 +1,53 @@
+import sys
+
 import pretty_midi
+from constants import ROLAND_DRUM_PITCH_CLASSES
+
+from pythonosc import udp_client
+import time
+
 import numpy as np
 # For plotting
 import mir_eval.display
 import librosa.display
 import matplotlib.pyplot as plt
 
+# load MIDI file
 pm = pretty_midi.PrettyMIDI('data/baron.mid')
 
-if (len(pm.instruments) == 1):
-    print('There are {} instruments. Please load a MIDI file with just one\
+if (len(pm.instruments) > 1):
+    sys.exit('There are {} instruments. Please load a MIDI file with just one\
  (drum) instrument track.'.format(len(pm.instruments)))
-print('There are {} time signature changes'.format(
-    len(pm.time_signature_changes)))
-print(pm.instruments[0].notes)
+
+# vars
+drumtrack = pm.instruments[0]
+if (drumtrack.is_drum == False):
+    sys.exit('Your MIDI file must be a DRUM track.')
+
+client = udp_client.SimpleUDPClient("127.0.0.1", 5005)
+
+# drum map
+
+
+def classes_to_map(classes):
+    class_map = {}
+    for cls, pitches in enumerate(classes):
+        for pitch in pitches:
+            class_map[pitch] = cls
+    return class_map
+
+
+pitch_class_map = classes_to_map(ROLAND_DRUM_PITCH_CLASSES)
+
+# parse drumtrack
+start = time.monotonic()
+for index, note in enumerate(drumtrack.notes):
+    print(pitch_class_map[note.pitch])
+    if index < (len(drumtrack.notes) - 1):
+        # if we're not at the last note, maybe wait
+        currstart = note.start
+        nextstart = drumtrack.notes[index + 1].start
+        sleeptime = nextstart - currstart
+    if (sleeptime):
+        client.send_message("/delay", 0)
+        time.sleep(sleeptime)
