4b7fe838b2d1a86b08808340cc1de7f77b59de74
RVirmoors <prowlergrig@yahoo.ie>
2023-02-11 17:19:30 +0200

    hooray, the libtorch model needs to warm up for 2-3 runs then runs super fast!!!

---
+++ cpp_app/main.cpp
@@ -0,0 +1,30 @@
+#include "torch/torch.h"
+#include "torch/script.h"
+#include <iostream>
+#include <chrono>
+
+using namespace std;
+
+int main() {
+    torch::jit::script::Module mod;
+    std::string m_path = "../../help/roly.ts";
+
+    try {
+        mod = torch::jit::load(m_path);
+        mod.eval();
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << '\n';
+    }
+
+    torch::Tensor output;
+
+    for (int i = 0; i < 15; i++) { 
+        torch::Tensor input = torch::rand({1, 5, 1});
+        auto start = std::chrono::high_resolution_clock::now();
+        output = mod.forward({input}).toTensor().detach();
+        auto end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+        cout << output[0][0][0] << endl;
+        cout << "duration: " << duration.count() / 1000. << " ms" << endl;
+    }
+}
