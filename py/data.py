fix feat scaling in train.py

py/data.py
@@ -46,20 +46,16 @@ def classes_to_map():
             class_map[pitch] = cls
     return class_map
 
-def ms_to_bartime(ms, featVec):
+def ms_to_bartime(ms: float, featVec):
     """
     Convert a ms time difference to a bar-relative diff.
     input:
         ms = time(s) to be converted [batch, seq_len, times_to_compute]
         featVec = feature of the note we relate to 
     """
-    if len(ms.shape) == 2:
-        ms = ms.unsqueeze(2)
-    tempo = featVec[:,:,9].unsqueeze(2)
-    timeSig = featVec[:,:,10].unsqueeze(2)
+    tempo = featVec[9]
+    timeSig = featVec[10]
     barDiff = ms / 1000. * 60. / tempo / timeSig
-    if barDiff.shape[2] == 1:
-        barDiff = barDiff.view(ms.shape[0], ms.shape[1])
 
     return barDiff
 
@@ -226,8 +222,8 @@ def readLiveOnset(input: torch.Tensor, x_dec: torch.Tensor, x_enc: torch.Tensor)
     while i >= 0:
         print("LOOKING FOR MATCH", input[:, 0, 2:5], x_enc[:, i, 9:12])
         if torch.allclose(input[:, 0, 2:5], x_enc[:, i, 9:12]):
-            x_dec[:, i, 13] = input[:, 0, 1]    # tau_guitar
-            print("FOUND MATCH")
+            x_dec[:, i, 13] = ms_to_bartime(input[:, 0, 1], x_enc[:, i].squeeze())   # tau_guitar
+            print("FOUND MATCH", x_dec[:, i, 13])
             return x_dec
         i -= 1
     return x_dec
@@ -244,17 +240,14 @@ def dataScaleDown(input: torch.Tensor):
     """
     Scale the input data to range [-1, 1].
 
-    tau_d, tau_g from ms to bartime
     9 velocities from [0, 127]
     bpm from [40, 240]
     tsig from [0.5, 1.5]
-    pos_in_bar from [0, 1]
+    pos_in_bar, tau_d, tau_g from [0, 1]
 
     input: (batch, vec_size, 14)
     output: (batch, vec_size, 14)
-    """
-    if input.shape[2] == 14:
-        input[:, :, 12:14] = ms_to_bartime(input[:, :, 12:14], input)
+    """       
     input[:, :, :9] = input[:, :, :9] / 63.5 - 1
     input[:, :, 9] = (input[:, :, 9] - 40) / 100 - 1
     input[:, :, 10] = input[:, :, 10] - 1
@@ -286,7 +279,6 @@ def dataScaleUp(input: torch.Tensor):
     return input
 
 
-
 # === TESTS ===
 
 if __name__ == '__main__':
@@ -309,6 +301,6 @@ if __name__ == '__main__':
     print(x_original, x)
     tolerance = 1e-3
     assert torch.allclose(x, x_original, atol=tolerance)
-    
+
     feat = x.squeeze(0)
     print(feat)

py/train.py
@@ -64,16 +64,20 @@ def getBatch(split, train_xd, val_xd, batch_size, block_size, train_xe=None, val
     else:
         xe = train_xe if split == 'train' else val_xe
 
-    xd = xd.clone().detach().to(device)
-    xe = xe.clone().detach().to(device)
-    data.dataScaleDown(xd)
-    data.dataScaleDown(xe)
+    if len(xd) == 0:
+        raise Exception("No data found in %s" % split)
+
+
 
     take_i = np.random.randint(0, len(xd), (batch_size))
     ix = [np.random.randint(0, xd[i].shape[0] - block_size) for i in take_i]
     x_dec = torch.stack([xd[take_i[i]][ix[i]:ix[i]+block_size] for i in take_i])
     x_enc = torch.stack([xe[take_i[i]][ix[i]:ix[i]+block_size] for i in take_i])
     y = torch.stack([xd[take_i[i]][ix[i]+1:ix[i]+block_size+1] for i in take_i])
+
+    data.dataScaleDown(x_enc)
+    data.dataScaleDown(x_dec)
+    data.dataScaleDown(y)
 
     if 'cuda' in device:
         # pin arrays x,y, which allows us to move them to GPU asynchronously (non_blocking=True)
@@ -91,7 +95,6 @@ def estimate_loss(model, train_xd, val_xd, batch_size, block_size, train_xe=None
         losses = torch.zeros(eval_iters)
         for k in range(eval_iters):
             X_enc, X_dec, Y = getBatch(split, train_xd, val_xd, batch_size, block_size, train_xe, val_xe)
-            X_enc, X_dec, Y = data.dataScaleDown(X_enc), data.dataScaleDown(X_dec), data.dataScaleDown(Y)
             y_hat = model(X_enc, X_dec)
             loss = model.loss(y_hat, Y)
             losses[k] = loss.item()
@@ -126,7 +129,7 @@ def train(model, config, load_model, epochs, train_xd, val_xd, batch_size, train
     scaler = torch.cuda.amp.GradScaler(enabled = (dtype == 'float16'))
 
     # training loop
-    X, Y = getBatch('train', train_xd, val_xd, batch_size, block_size, train_xe, val_xe) # get first batch
+    X_enc, X_dec, Y = getBatch('train', train_xd, val_xd, batch_size, block_size, train_xe, val_xe) # get first batch
     t0 = time.time()
     local_iter_num = 0 # number of iterations in the lifetime of this process
     #for epoch in range(epochs):
@@ -138,7 +141,7 @@ def train(model, config, load_model, epochs, train_xd, val_xd, batch_size, train
         
         if iter_num % eval_interval == 0:
             losses = estimate_loss(model, train_xd, val_xd, batch_size, block_size, train_xe, val_xe)
-            print(f"step {iter_num}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+            print(f"step {iter_num}: train loss {losses['train']:.6f}, val loss {losses['val']:.6f}")
             if losses['val'] < best_val_loss:
                 best_val_loss = losses['val']
                 torch.save(model.state_dict(), f'{out_dir}/model_best.pt')
@@ -156,10 +159,10 @@ def train(model, config, load_model, epochs, train_xd, val_xd, batch_size, train
         # forward backward update, with optional gradient accumulation to simulate larger batch size
         # and using the GradScaler if data type is float16
         for micro_step in range(gradient_accumulation_steps):
-            Y_hat = model(X, Y)
+            Y_hat = model(X_enc, X_dec)
             loss = model.loss(Y_hat, Y)
             # immediately async prefetch next batch while model is doing the forward pass on the GPU
-            X, Y = getBatch('train', train_xd, val_xd, batch_size, block_size, train_xe, val_xe)
+            X_enc, X_dec, Y = getBatch('train', train_xd, val_xd, batch_size, block_size, train_xe, val_xe)
             if dtype == 'float16':
                 scaler.scale(loss).backward()
             else:
@@ -184,7 +187,7 @@ def train(model, config, load_model, epochs, train_xd, val_xd, batch_size, train
         t0 = t1
         if iter_num % log_interval == 0:
             lossf = loss.item()
-            print(f"step {iter_num}: train loss {lossf:.4f}, lr {lr:.4f}, {dt:.2f}s")
+            print(f"step {iter_num}: train loss {lossf:.6f}, lr {lr:.6f}, {dt:.2f}s")
 
         iter_num += 1
         local_iter_num += 1