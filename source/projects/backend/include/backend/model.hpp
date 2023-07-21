24c3f26a6f3403b33f8c3682f2a31a68d2dc14bf
grig <prowlergrig@yahoo.ie>
2023-07-21 18:33:25 +0300

    position encoding to use just bar_pos

--- source/projects/backend/include/backend/model.hpp
+++ source/projects/backend/include/backend/model.hpp
@@ -6,11 +6,12 @@
 namespace backend {
 
 using namespace torch;
+using namespace at::indexing;
 
 struct TransformerModelImpl : nn::Module {
     TransformerModelImpl(int input_dim, int output_dim, int d_model, int nhead, torch::Device device) :
     device(device),
-    pos_linLayer(nn::Linear(input_dim, d_model)),
+    pos_linLayer(nn::Linear(1, d_model)),
     embedding(nn::Linear(input_dim, d_model)),
     transformer(nn::Transformer(nn::TransformerOptions(d_model, nhead))),
     fc(nn::Linear(d_model, output_dim))
@@ -26,21 +27,20 @@ struct TransformerModelImpl : nn::Module {
     }
 
     torch::Tensor generatePE(torch::Tensor x) {
-        return pos_linLayer(x);
+        return pos_linLayer(x.index({Slice(), Slice(), INX_BAR_POS}).unsqueeze(2));
     }
 
     torch::Tensor forward(torch::Tensor src, torch::Tensor tgt) {
-        bool noBatch = false;
-        torch::Tensor src_posenc = generatePE(src);
-        torch::Tensor tgt_posenc = generatePE(tgt);
-
-        src = embedding(src) + src_posenc;
-        tgt = embedding(tgt) + tgt_posenc;
+        bool noBatch = false;        
         if (src.sizes().size() == 2) {
             src.unsqueeze_(0); // add batch dimension, needed for transformer
             tgt.unsqueeze_(0); // (B, T, C)
             noBatch = true;
         }
+        torch::Tensor src_posenc = generatePE(src);
+        torch::Tensor tgt_posenc = generatePE(tgt);
+        src = embedding(src) + src_posenc;
+        tgt = embedding(tgt) + tgt_posenc;
         torch::Tensor src_mask = transformer->generate_square_subsequent_mask(src.size(1)).to(device);
         // (B, T, C) -> (T, B, C)
         src.transpose_(0, 1);
