3689ea7fe981e2e392d8180ca2fa1caeff3fa4f0
grig <prowlergrig@yahoo.ie>
2023-08-26 18:17:45 +0300

    2.0.1 release

---
+++ py/make_release.py
@@ -0,0 +1,34 @@
+import os
+import zipfile
+
+def zip_files_and_folders(input_list, output_zip):
+    with zipfile.ZipFile(output_zip, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        for item in input_list:
+            if os.path.isfile(item):
+                # If the item is a file, add it to the zip with the 'rolypoly' directory structure.
+                arcname = os.path.join('rolypoly/py', os.path.basename(item))
+                zipf.write(item, arcname=arcname)
+            elif os.path.isdir(item):
+                # If the item is a directory, add its contents to the zip with the 'rolypoly' directory structure.
+                for root, _, files in os.walk(item):
+                    for file in files:
+                        arcname = os.path.join('rolypoly/py', os.path.relpath(os.path.join(root, file)))
+                        print(arcname)
+                        zipf.write(os.path.join(root, file), arcname=arcname)
+
+if __name__ == "__main__":
+    input_list = ['../docs',
+                  '../externals',
+                  '../extras',
+                  '../help',
+                  '../patchers',
+                  '../support',
+                  '../package-info.json',
+                  '../License.md',
+                  '../README.md',
+                  '../icon.png'
+                  ]
+    output_zip = 'rolypoly-v2.0.1.zip' 
+
+    zip_files_and_folders(input_list, output_zip)
+    print(f'Successfully created {output_zip}')
